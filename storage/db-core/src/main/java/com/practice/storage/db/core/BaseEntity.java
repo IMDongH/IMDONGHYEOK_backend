a2a5959808aab7c7ead50176cf36a18529e2d826
임동혁 <idh1007@naver.com>
2025-12-31 17:19:20 +0900

    feat(storage): db-core 모듈 설정 및 BaseEntity 추가

---
+++ storage/db-core/src/main/java/com/practice/storage/db/core/BaseEntity.java
@@ -0,0 +1,66 @@
+package com.practice.storage.db.core;
+
+import com.practice.core.enums.EntityStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private EntityStatus status = EntityStatus.ACTIVE;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStatus(EntityStatus status) {
+        this.status = status;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void active() {
+        this.status = EntityStatus.ACTIVE;
+    }
+
+    public boolean isActive() {
+        return this.status == EntityStatus.ACTIVE;
+    }
+
+    public void delete() {
+        this.status = EntityStatus.DELETED;
+    }
+
+    public boolean isDeleted() {
+        return this.status == EntityStatus.DELETED;
+    }
+}
