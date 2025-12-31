1b90cf260c3d4d5e340c715f3ef8120eaa709e25
임동혁 <idh1007@naver.com>
2025-12-31 17:08:17 +0900

    chore(core): 프로젝트 초기 구조 및 CoreApplication 설정
    - Gradle 빌드 설정 및 Wrapper 추가
    - 멀티 모듈 프로젝트 구조 설정 (settings.gradle)
    - CoreApplication 메인 클래스 추가
    - .gitmessage 템플릿 추가

---
+++ core/core-api/src/main/java/com/practice/core/CoreApplication.java
@@ -0,0 +1,11 @@
+package com.practice.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.practice")
+public class CoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
