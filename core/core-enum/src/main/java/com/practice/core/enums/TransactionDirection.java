807105157ab6819f1f53d6c31809b732458965fb
임동혁 <idh1007@naver.com>
2025-12-31 17:37:11 +0900

    feat(core-enum): TransactionType 및 Direction Enum 추가

---
+++ core/core-enum/src/main/java/com/practice/core/enums/TransactionDirection.java
@@ -0,0 +1,6 @@
+package com.practice.core.enums;
+
+public enum TransactionDirection {
+    IN,
+    OUT
+}
---
+++ core/core-enum/src/main/java/com/practice/core/enums/TransactionType.java
@@ -0,0 +1,7 @@
+package com.practice.core.enums;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAW,
+    TRANSFER
+}
